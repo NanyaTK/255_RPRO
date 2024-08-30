edb85f7d5ca55b88766397549d1cfa55649d1e15
NanyaTK <herenfuyong02@gmail.com>
2024-08-30 22:18:39 +0900

    feat: add cd

--- src/sw.js
+++ src/sw.js
@@ -1,5 +1,5 @@
 const addResourcesToCache = async (resources) => {
-    const cache = await caches.open("v1");
+    const cache = await caches.open("v2");
     await cache.addAll(resources);
 }
 
@@ -40,4 +40,20 @@ addEventListener("fetch", (event) => {
             return fetch(event.request);
         })(),
     );
+});
+
+const deleteCache = async (key) => {
+    await caches.delete(key);
+};
+
+const deleteOldCaches = async () => {
+    const cacheKeepList = ["v2"];
+    const keyList = await caches.keys();
+    const cachesToDelete = keyList.filter((key) => !cacheKeepList.includes(key));
+    await Promise.all(cachesToDelete.map(deleteCache));
+};
+
+self.addEventListener("activate", (event) => {
+    event.waitUntil(deleteOldCaches());
+    console.log("[process: SW] old caches deleted");
 });
