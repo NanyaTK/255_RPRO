5acd4abcfd0d61039d5949e7c10d14bedd1d22d0
NanyaTK <herenfuyong02@gmail.com>
2024-08-30 22:34:48 +0900

    feat: del cache "v1"

--- src/sw.js
+++ src/sw.js
@@ -42,18 +42,7 @@ addEventListener("fetch", (event) => {
     );
 });
 
-const deleteCache = async (key) => {
-    await caches.delete(key);
-};
-
-const deleteOldCaches = async () => {
-    const cacheKeepList = ["v2"];
-    const keyList = await caches.keys();
-    const cachesToDelete = keyList.filter((key) => !cacheKeepList.includes(key));
-    await Promise.all(cachesToDelete.map(deleteCache));
-};
-
 self.addEventListener("activate", (event) => {
-    event.waitUntil(deleteOldCaches());
+    event.waitUntil(caches.delete("v1"));
     console.log("[process: SW] old caches deleted");
 });
