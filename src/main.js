f6ef4f6383c5712337ee4e35ac737af767944ddf
NanyaTK <herenfuyong02@gmail.com>
2024-08-31 22:29:04 +0900

    feat: add test

--- src/main.js
+++ src/main.js
@@ -64,7 +64,7 @@ unregisterSW.addEventListener("click", () => {
         }
     });
     deleteAllCachesByManual();
-    window.location.reload();
+    //window.location.reload();
 });
 
 /* ============================================================== */
