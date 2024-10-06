09622bd51e836d11e0f5c2df5dc4be0d3291cc9b
marron <ritsumarokaho@gmail.com>
2024-10-06 22:30:50 +0900

    v1.3.1

--- src/sw.js
+++ src/sw.js
@@ -27,7 +27,7 @@
  * @param {*} resources File paths to be cached
  */
 const addResourcesToCache = async (resources) => {
-    const cache = await caches.open("v1.1.2");
+    const cache = await caches.open("v1.3.1");
     await cache.addAll(resources);
 }
 
