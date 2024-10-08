b9be2f7dd596a08437f5ceaa87bebc89d7b674ea
amedama-tech <ai7880096@gmail.com>
2024-10-08 10:52:21 +0900

    feat- 登録した時間割配列の先頭が0（テンプレ）だった時に、先頭要素を変えない

--- src/main.js
+++ src/main.js
@@ -247,7 +247,9 @@ function getAllSelectedOptionIds() {
         registDatas.push(registData);
     }
 
-    registDatas.unshift("1");
+    if(registDatas[0] == "0"){
+        registDatas.unshift("1");
+    }
 
     console.log("[process: main] " + registDatas);
     const registJSON = JSON.stringify(registDatas);
