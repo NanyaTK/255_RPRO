0364af40310f318b05fee5788a9783d017432a86
NayaTK-NP <herenfuyong02@gmail.com>
2024-10-10 16:58:12 +0900

    v1.3.7

--- src/main.php
+++ src/main.php
@@ -19,7 +19,7 @@
  * 
  * main.php is the main file of RPRO app.
  */
-define("APPLICCATION_VERSION", "v1.3.6");
+define("APPLICCATION_VERSION", "v1.3.7");
 
 // POSTされたデータを取得
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
