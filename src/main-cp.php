f18b20b76f163f82951840055e87859a8badcbc9
amedama-tech <ai7880096@gmail.com>
2024-10-13 21:14:23 +0900

    feat- subjects に dataを代入

--- src/main-cp.php
+++ src/main-cp.php
@@ -25,6 +25,7 @@ define("APPLICCATION_VERSION", "v1.1.2");
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $data = json_decode(file_get_contents('php://input'), true);
     $data = explode(",", $data);
+    $subjects = $data;
     foreach ($subjects as &$subject) {
         // cs- を削除
         $subject = substr($subject, 3);
