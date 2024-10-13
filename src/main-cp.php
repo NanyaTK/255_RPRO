b77351c85f7c3552dd50e1ed1e0ced419c1e2a66
amedama-tech <ai7880096@gmail.com>
2024-10-13 17:02:58 +0900

    feat- 恐らく不要な部分の削除

--- src/main-cp.php
+++ src/main-cp.php
@@ -34,218 +34,77 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <!DOCTYPE html>
 <html lang="ja">
 
-<?php
-$mysqli = new mysqli("127.0.0.1", "rpro_u", "uhe6WTScplbJ", "rpro", 3306);
-if ($mysqli->connect_error) {
-    echo $mysqli->connect_error;
-    exit();
-} else {
-    $mysqli->set_charset("utf8");
-}
-$mysqli->query("use rpro");
-$result = $mysqli->prepare(
-    "SELECT
-    `ID`
-    ,`科目ID`
-    , `学科ID`
-    , `科目名`
-    , `講義回数`
-    , `最大欠席可能回数`
-    , `特殊欠席条件`
-    , `評価割合`
-    , `科目分類`
-FROM
-    rpro.classtable
-ORDER BY
-    `ID` DESC"
-);
-$result->execute();
-$time_schdule = $result->get_result();
-$row_data = $time_schdule->fetch_array(MYSQLI_NUM);
-
-$mysqli->close();
-?>
-
-<head>
-    <meta charset="UTF-8" />
-    <title>留年プロテクター</title>
-    <link rel="icon" href="/icon-images/favicon.ico">
-    <link rel="stylesheet" href="main.css" />
-    <script type="text/javascript" src="sw.js"></script>
-    <link rel="manifest" href="mainManifest.json" />
-    <meta name="google-site-verification" content="E3maZI8wva9G9nRwR8SETlWMM2MSqnCULOvfpkELHsI" />
-</head>
-
 <body>
-    <header>
-        <div class="flex-byForce">
-            <a class="header" href="/main.php">留年プロテクター <?php echo APPLICCATION_VERSION ?></a>
-            <div class="menu-icon" onclick="toggleMenu()">&#9776;</div>
-            <nav id="menu" class="menu">
-                <ul>
-                    <li><a href="help.php">よくある質問</a></li>
-                    <li><button id="install-btn">インストール</button></li>
-                    <li> <button id="uninstall-btn">再起動</button>
-                    </li>
-                </ul>
-            </nav>
-        </div>
-    </header>
-    <div class="content">
+    <div class="jikanwari">
+        <?php
+        // ここから時間割表示
+        if (!$subjects) {
+            // POSTデータが無い時の時間割データの初期値
+            $subjects = ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""];
+        }
+        foreach ($subjects as &$subject) {
+            $subject = substr($subject, -2);
+        }
+        // 曜日と時間割の初期データ
+        $days = ['月', '火', '水', '木', '金'];
+        $times = ['1', '2', '3', '4'];
 
-        <div class="main">
-            <div class="flex-byForce">
-                <button id="signup-btn">新規登録</button>
-            </div>
-            <?php // ここから新規登録画面
-            ?>
-            <div id="popup-wrapper">
-                <div id="popup-inside">
-                    <div id="close">&times;</div>
-                    <div id="message">
-                        <h2>新規登録</h2>
-                        <p>時間割を設定してください</p>
-                        <div class="flex-byForce">
-                            <label class="select-class">
-                                <select class="auto-complete">
-                                    <option id=13>電気情報工学科電気電子コース4年</option>
-                                    <option id=14>電気情報工学科情報工学コース4年</option>
-                                    <option id=17>電気情報工学科電気電子コース5年</option>
-                                    <option id=18>電気情報工学科情報工学コース5年</option>
-                                </select>
-                            </label>
-                            <label class="select-class term-sel-label">
-                                <select class="term-sel">
-                                    <option id=1>前期</option>
-                                    <option id=2>後期</option>
-                                </select>
-                            </label>
-                            <button id="cltemp-btn" class="clt-fil-btn filDB">絞り込み</button>
-                            <button id="rstFilter-btn" class="clt-fil-btn">リセット</button>
-                        </div>
-                        <div class="jikanwari">
-                            <?php
-                            // 曜日と時間割の初期データ
-                            $days = ['月', '火', '水', '木', '金'];
-                            $times = ['1', '2', '3', '4'];
-                            ?>
-                            <table class="timetable-signup">
-                                <tr>
-                                    <th class="day-column">曜日</th>
-                                    <?php foreach ($times as $time) : ?>
-                                        <th><?php echo $time; ?></th>
-                                    <?php endforeach; ?>
-                                </tr>
-                                <?php foreach ($days as $day) : ?>
-                                    <tr>
-                                        <td class="day-column"><?php echo $day; ?></td>
-                                        <?php foreach ($times as $timeIndex => $time) : ?>
-                                            <td class="time-cell">
-                                                <label class="select-subject">
-                                                    <?php
-                                                    // selectタグのidを動的に生成
-                                                    $selectId = 'mys-' . $day . '-' . $timeIndex;
-                                                    ?>
-                                                    <select id="<?php echo $selectId; ?>" class="subject-select">
-                                                        <option class="empty">空コマ</option>
-                                                        <?php
-                                                        for ($row_no = $time_schdule->num_rows - 1; $row_no >= 0; $row_no--) {
-                                                            $time_schdule->data_seek($row_no);
-                                                            $row = $time_schdule->fetch_assoc();
-                                                            echo '<option id ="cs-' . $row["ID"] . '" class="c-' . $row["学科ID"] . '">' . $row["科目名"] . '</option>';
-                                                        }
-                                                        ?>
-                                                    </select>
-                                                </label>
-                                            </td>
-                                        <?php endforeach; ?>
-                                    </tr>
-                                <?php endforeach; ?>
-                            </table>
-                        </div>
-                        <button id="finalize-btn" onclick="getAllSelectedOptionIds()">確定する</button>
-                        <!--
-                        <form method="POST" action="main.php" id="hiddenForm">
-                            <input type="hidden" name="jsData" id="jsData">
-                            <button id="finalize-btn" onclick="getAllSelectedOptionIds()" type="submit">確定する</button>
-                        </form>
-                        --->
-                        <p id="result"></p>
-                    </div>
-                </div>
-            </div>
-            <?php // ここまで新規登録画面
-            ?>
-            <div class="jikanwari">
+        // 1週間の時間割の科目数（曜日数×時間数）
+        $subjectsPerDay = count($subjects) / count($days);
+
+        // 各曜日ごとに科目を分割
+        $subjectsByDay = array_chunk($subjects, $subjectsPerDay);
+        ?>
+        <script>
+            console.log('[process: main] subjects finish');
+        </script>
+        <table class="timetable">
+            <tr>
+                <th class="day-column">曜日</th>
                 <?php
-                // ここから時間割表示
-                if (!$subjects) {
-                    // POSTデータが無い時の時間割データの初期値
-                    $subjects = ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""];
-                }
-                foreach ($subjects as &$subject) {
-                    $subject = substr($subject, -2);
-                }
-                // 曜日と時間割の初期データ
-                $days = ['月', '火', '水', '木', '金'];
-                $times = ['1', '2', '3', '4'];
-
-                // 1週間の時間割の科目数（曜日数×時間数）
-                $subjectsPerDay = count($subjects) / count($days);
-
-                // 各曜日ごとに科目を分割
-                $subjectsByDay = array_chunk($subjects, $subjectsPerDay);
-                ?>
-                <script>
-                    console.log('[process: main] subjects finish');
-                </script>
-                <table class="timetable">
-                    <tr>
-                        <th class="day-column">曜日</th>
-                        <?php
-                        $howmanyA = 0;
-                        $howmanyB = 0;
-                        foreach ($times as $time) : ?>
-                            <th><?php echo $time; ?></th>
-                        <?php endforeach; ?>
-                    </tr>
-                    <?php
-                    foreach ($days as $index => $day) : ?>
-                        <tr>
-                            <td class="day-column"><?php echo $day; ?></td>
-                            <?php foreach ($times as $timeIndex) : ?>
-                                <td class="time-cell">
-                                    <?php
-
-                                    // 科目名を取得
-                                    if ($subjectsByDay[$index][$timeIndex - 1]) {
-                                        if ($subjectsByDay[$index][$timeIndex - 1] == 1) {
-                                            $row_no = $time_schdule->num_rows - $subjectsByDay[$index][$timeIndex - 1];
-                                        } else {
-                                            $row_no = $time_schdule->num_rows - $subjectsByDay[$index][$timeIndex - 1] + 1;
-                                        }
-                                        $time_schdule->data_seek($row_no);
-                                        $row = $time_schdule->fetch_assoc();
-                                        $subjectName = $row["科目名"];
-                                        $subjectId = $row["科目ID"]; // 科目ごとのIDを使う  
-                                        echo ('<button id="absenceButton_' . $subjectId . '" class ="open-popup-btn-' . $howmanyA . ' subject" data-subject-id=' . $subjectId . '>');
-                                        echo ($row["科目名"]);
-                                        $subjectName = $row["科目名"];
-                                        $subjectId = $row["科目ID"]; // 科目ごとのIDを使う
-                                    } else {
-                                        echo ('<button class ="open-popup-btn-' . $howmanyA . ' subject" data-subject-id=' . $subjectId . '>');
-                                        echo isset($subjectsByDay[$index][$timeIndex - 1]) ? $subjectsByDay[$index][$timeIndex - 1] : '';
-                                        $subjectName = isset($subjectsByDay[$index][$timeIndex - 1]) ? $subjectsByDay[$index][$timeIndex - 1] : '';
-                                        $subjectId = $index . '-' . $timeIndex; // 科目IDがない場合はデフォルトのIDを作る
-                                    }
-                                    ?>
-                                    </button>
-                                    <p>欠席回数 <span id="absenceCount_<?php echo $howmanyA; ?>">0</span> / 最大欠席回数</p>
-                                    <?php
-                                    $howmanyA += 1; ?>
-                                </td>
-                            <?php endforeach; ?>
-                        </tr>
+                $howmanyA = 0;
+                $howmanyB = 0;
+                foreach ($times as $time) : ?>
+                    <th><?php echo $time; ?></th>
+                <?php endforeach; ?>
+            </tr>
+            <?php
+            foreach ($days as $index => $day) : ?>
+                <tr>
+                    <td class="day-column"><?php echo $day; ?></td>
+                    <?php foreach ($times as $timeIndex) : ?>
+                        <td class="time-cell">
+                            <?php
+                            // 科目名を取得
+                            if ($subjectsByDay[$index][$timeIndex - 1]) {
+                                if ($subjectsByDay[$index][$timeIndex - 1] == 1) {
+                                    $row_no = $time_schdule->num_rows - $subjectsByDay[$index][$timeIndex - 1];
+                                } else {
+                                    $row_no = $time_schdule->num_rows - $subjectsByDay[$index][$timeIndex - 1] + 1;
+                                }
+                                $time_schdule->data_seek($row_no);
+                                $row = $time_schdule->fetch_assoc();
+                                $subjectName = $row["科目名"];
+                                $subjectId = $row["科目ID"]; // 科目ごとのIDを使う  
+                                echo ('<button id="absenceButton_' . $subjectId . '" class ="open-popup-btn-' . $howmanyA . ' subject" data-subject-id=' . $subjectId . '>');
+                                echo ($row["科目名"]);
+                                $subjectName = $row["科目名"];
+                                $subjectId = $row["科目ID"]; // 科目ごとのIDを使う
+                            } else {
+                                echo ('<button class ="open-popup-btn-' . $howmanyA . ' subject" data-subject-id=' . $subjectId . '>');
+                                echo isset($subjectsByDay[$index][$timeIndex - 1]) ? $subjectsByDay[$index][$timeIndex - 1] : '';
+                                $subjectName = isset($subjectsByDay[$index][$timeIndex - 1]) ? $subjectsByDay[$index][$timeIndex - 1] : '';
+                                $subjectId = $index . '-' . $timeIndex; // 科目IDがない場合はデフォルトのIDを作る
+                            }
+                            ?>
+                            </button>
+                            <p>欠席回数 <span id="absenceCount_<?php echo $howmanyA; ?>">0</span> / 最大欠席回数</p>
+                            <?php
+                            $howmanyA += 1; ?>
+                        </td>
                     <?php endforeach; ?>
-                </table>
-            </div>
+                </tr>
+            <?php endforeach; ?>
+        </table>
+    </div>
+</body>
