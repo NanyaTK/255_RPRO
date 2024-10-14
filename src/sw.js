9b8bf31eebd40eef2e727747f97382a218ddad35
NanyaTK <herenfuyong02@gmail.com>
2024-10-14 18:27:54 +0900

    v1.3.8

--- src/sw.js
+++ src/sw.js
@@ -20,7 +20,7 @@
  * sw.js is the abbreviation for ServiceWorker.js
  * This file includes function of PWA.
  */
-const APPLICCATION_VERSION = "v1.3.7"
+const APPLICCATION_VERSION = "v1.3.8"
 
 /**
  * The file path to be cached passed in the resource
